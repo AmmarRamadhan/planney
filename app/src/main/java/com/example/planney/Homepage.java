353939e89a07c092d347d55cdcda9eb3b762b825
Nicholas <nicholas>
2022-10-12 23:08:54 +0800

    Adding Homepage

---
+++ app/src/main/java/com/example/planney/Homepage.java
@@ -0,0 +1,33 @@
+package com.example.planney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+
+    public void Tab1to2(View view) {
+        Intent intent = new Intent(Homepage.this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void Tab1to3(View view) {
+        Intent intent = new Intent(Homepage.this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void Tab1to4(View view) {
+        Intent intent = new Intent(Homepage.this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void Tab1to5(View view) {
+        Intent intent = new Intent(Homepage.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
